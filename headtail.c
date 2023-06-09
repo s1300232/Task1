fea511fd49c14c4950db7501cdc993282b9c06f6
s1300232 <136040069+s1300232@users.noreply.github.com>
2023-06-09 12:01:13 +0900

    Dice game: initial revision

---
+++ headtail.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int i, num, heads=0, tails=0,sum=0;
+
+  printf("Rolling dice...\n");
+  srand((unsigned int)time(NULL));
+  for(i=0; i<2; i++){
+   
+    num = rand()%6+1;
+    printf("Die %d: %d\n", i+1,num);
+    sum= sum+ num;
+
+  }
+  printf("Total value: %d\n",sum);
+
+
+
+  return 0;
+}
