e82feddf0e73309aea74b65b64057cc9717d7097
s1300232 <136040069+s1300232@users.noreply.github.com>
2023-06-09 12:17:35 +0900

    Greetings added

---
+++ greet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int i, num, heads=0, tails=0,sum=0;
+  char name[10];
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
+  printf("Rolling dice...\n");
+  srand((unsigned int)time(NULL));
+  for(i=0; i<2; i++){
+   
+    num = rand()%6+1;
+    printf("Die %d: %d\n", i+1,num);
+    sum= sum+ num;
+
+  }
+  printf("Total value: %d\n",sum);
+
+
+
+  return 0;
+}
