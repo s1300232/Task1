f00cd239b14f734acade9f6b4bdea83af35987fc
s1300232 <136040069+s1300232@users.noreply.github.com>
2023-06-09 12:33:29 +0900

    Won/lost message modified

--- headtail.c
+++ headtail.c
@@ -21,10 +21,10 @@ int main(){
   printf("Total value: %d\n",sum);
 
  if(sum>7){
-    printf("You won\n");
+    printf("%s won!\n",name);
   }
   else{
-    printf("You lost\n");
+    printf("%s lost!\n",name);
   }
 
 
